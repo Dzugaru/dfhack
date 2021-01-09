db96e190d6bebbd4a25bcffa4e99ecd4b7db91aa
Dzugaru <dzugaru@gmail.com>
2021-01-09 15:41:51 +0300

    funtools/autoforbidboulders

--- plugins/funtools.cpp
+++ plugins/funtools.cpp
@@ -6,13 +6,17 @@
 #include "Console.h"
 #include "Export.h"
 #include "PluginManager.h"
+#include "modules/EventManager.h"
 #include "modules/Maps.h"
 #include "modules/Units.h"
 #include "modules/World.h"
 #include "modules/MapCache.h"
 #include "modules/Gui.h"
 #include "modules/Job.h"
+#include "MiscUtils.h"
+#include "uicommon.h"
 #include <algorithm>
+#include <unordered_set>
 
 #include "df/block_square_event_frozen_liquidst.h"
 #include "df/construction.h"
@@ -21,6 +25,8 @@
 #include "df/announcements.h"
 #include "df/unit.h"
 #include "df/general_ref_unit_workerst.h"
+#include "df/viewscreen_dwarfmodest.h"
+#include "df/ui_look_list.h"
 
 using MapExtras::MapCache;
 
@@ -31,9 +37,16 @@ using namespace DFHack;
 using namespace df::enums;
 
 DFHACK_PLUGIN("funtools");
-DFHACK_PLUGIN_IS_ENABLED(is_active);
+DFHACK_PLUGIN_IS_ENABLED(is_enabled);
 
 REQUIRE_GLOBAL(world);
+REQUIRE_GLOBAL(process_dig);
+REQUIRE_GLOBAL(ui);
+REQUIRE_GLOBAL(ui_look_list);
+REQUIRE_GLOBAL(ui_look_cursor);
+
+void onDig(color_ostream& out, void* ptr);
+EventManager::EventHandler digHandler(onDig, 0);
 
 bool nodigcancel_state = false;
 
@@ -52,37 +65,8 @@ int last_dig_cancel_announce_id = -1;
 int last_dig_cancel_repeats = -1;
 void nodigcancel_update(color_ostream& out);
 
-DFhackCExport command_result plugin_init ( color_ostream &out, vector <PluginCommand> &commands)
-{   
-    //Reverts back dig designation if damp stone is detected and unpauses the game.
-    //Caution! Use only when digging in aquifer.
-    //Saves all dig designated tiles every frame and rollbacks the designation when it detects new DIG_DAMP_STONE announcement.
-    //This, however, wont restore dwarf dig job, so a dwarf can be confused sometimes before he picks up the restored dig designation again.
-    //(its basically the same if you just reassign the designation youself as usual)
-    commands.push_back(PluginCommand("nodigcancel", "Disable dig cancellation on damp stone (useful when digging in aquifer)", nodigcancel, false,
-        "Disable dig cancellation on damp stone (useful when digging in aquifer)\n"
-        "Activate with 'nodigcancel 1', deactivate with 'nodigcancel 0'.\n"));
-    return CR_OK;
-}
-
-DFhackCExport command_result plugin_onupdate ( color_ostream &out )
-{   
-    t_gamemodes gm;
-    World::ReadGameMode(gm);
-    if(gm.g_mode == game_mode::DWARF)
-    {
-        if (nodigcancel_state)
-        {
-            nodigcancel_update(out);         
-        }
-    }
-    return CR_OK;
-}
-
-DFhackCExport command_result plugin_shutdown ( color_ostream &out )
-{
-    return CR_OK;
-}
+//TODO: save/load this from file
+std::unordered_set<std::string> forbid_freshly_mined;
 
 command_result nodigcancel(color_ostream& out, vector <string>& parameters)
 {
@@ -91,8 +75,7 @@ command_result nodigcancel(color_ostream& out, vector <string>& parameters)
         if (parameters[0] == "0")
             nodigcancel_state = 0;
         else
-            nodigcancel_state = 1;   
-        is_active = nodigcancel_state;
+            nodigcancel_state = 1;           
         out.print("nodigcancel %sactivated.\n", (nodigcancel_state ? "" : "de"));
     }
     else
@@ -150,7 +133,7 @@ void nodigcancel_save(color_ostream& out)
     }
 }
 
-int nodigcancel_dist(df::coord a, df::coord b)
+int chebyshev_dist(df::coord a, df::coord b)
 {
     return max(max(abs(a.x - b.x), abs(a.y - b.y)), abs(a.z - b.z));
 }
@@ -185,13 +168,13 @@ void nodigcancel_fix(color_ostream& out)
                 if (!DFHack::Units::isActive(u))
                     continue;
 
-                if (nodigcancel_dist(u->pos, c) <= 2) //nearby digger...
+                if (chebyshev_dist(u->pos, c) <= 2) //nearby digger...
                 {
                     bool found_digger = false;
                     for each (auto job in saved_dig_jobs)
                     {
                         if (Job::getWorker(job)->id == u->id && (u->job.current_job == NULL || u->job.current_job->id != job->id) &&
-                            nodigcancel_dist(job->pos, c) == 1) //...who had nearby dig job
+                            chebyshev_dist(job->pos, c) == 1) //...who had nearby dig job
                         {
                             //TODO: ideally we want to set back his canceled job, but thats rather tricky,
                             //so lets just cancel his new pathfinding for now if he has already something other to do
@@ -203,6 +186,10 @@ void nodigcancel_fix(color_ostream& out)
                             u->path.dest.x = -1;
                             u->path.dest.y = -1;
                             u->path.dest.z = -1;
+                            u->job.current_job == NULL;
+                            *process_dig = true;
+
+                            out.print("resetted nearby dwarf work\n");
 
                             found_digger = true;
                             break;
@@ -243,4 +230,144 @@ void nodigcancel_update(color_ostream& out)
     nodigcancel_init = true;    
 }
 
+void onDig(color_ostream& out, void* ptr)
+{
+    CoreSuspender alice;
+    df::job* job = (df::job*)ptr;
+    if (job->completion_timer > 0)
+        return;
+
+    if (job->job_type != df::enums::job_type::Dig &&
+        job->job_type != df::enums::job_type::CarveUpwardStaircase &&
+        job->job_type != df::enums::job_type::CarveDownwardStaircase &&
+        job->job_type != df::enums::job_type::CarveUpDownStaircase &&
+        job->job_type != df::enums::job_type::CarveRamp &&
+        job->job_type != df::enums::job_type::DigChannel)
+        return;
+
+    auto& items = world->items.other[items_other_id::IN_PLAY];
+    for (size_t i = 0; i < items.size(); i++)
+    {
+        df::item* item = items[i];
+
+        if (item->flags.bits.forbid)
+            continue;
+
+        //TODO: refine this, research where the boulder can appear (digChannel?)
+        //and what age can it be? 0 or more?
+        if (chebyshev_dist(item->pos, job->pos) > 1 || item->age > 3)
+            continue;
+        ItemTypeInfo itinfo(item);
+        if (itinfo.type != df::enums::item_type::BOULDER)
+            continue;
+        MaterialInfo minfo(item);
+        if (forbid_freshly_mined.count(minfo.toString()) == 0)
+            continue;
+
+        item->flags.bits.forbid = 1;
+    }    
+}
+
+struct dwarfmode_hook : public df::viewscreen_dwarfmodest
+{
+    typedef df::viewscreen_dwarfmodest interpose_base;    
+    
+    static std::string getSelectedBoulderMat()
+    {
+        if (ui->main.mode != df::ui_sidebar_mode::LookAround)
+            return std::string();
+        auto el = vector_get(ui_look_list->items, *ui_look_cursor);
+        if (el == NULL || el->type != df::ui_look_list::T_items::T_type::Item)
+            return std::string();
+        auto item = el->data.Item;
+        ItemTypeInfo itinfo(item);
+        if (itinfo.type != df::enums::item_type::BOULDER)
+            return std::string();
+        MaterialInfo minfo(item);
+        return minfo.toString();
+    }
+
+    DEFINE_VMETHOD_INTERPOSE(void, feed, (set<df::interface_key>* input))
+    {
+        if (input->count(interface_key::CUSTOM_ALT_F))
+        {
+            auto mat = getSelectedBoulderMat();
+            if (mat.empty())
+                return;
+
+            bool isEnabled = forbid_freshly_mined.count(mat) == 1;
+            if (!isEnabled)
+                forbid_freshly_mined.insert(mat);
+            else
+                forbid_freshly_mined.erase(mat);
+        }
+        INTERPOSE_NEXT(feed)(input);
+    }
+
+    DEFINE_VMETHOD_INTERPOSE(void, render, ())
+    {
+        INTERPOSE_NEXT(render)();
+
+        auto mat = getSelectedBoulderMat();
+        if (mat.empty())
+            return;
+
+        bool isEnabled = forbid_freshly_mined.count(mat) == 1;
+
+        auto dims = Gui::getDwarfmodeViewDims();
+        int left_margin = dims.menu_x1 + 1;
+        int x = left_margin;
+        int y = dims.y2;
+        
+        auto text = isEnabled ? "autoforbid: on" : "autoforbid: off";
+        OutputHotkeyString(x, y, text, "Alt+f", false, 0, COLOR_WHITE, COLOR_LIGHTRED);
+    }
+};
+
+IMPLEMENT_VMETHOD_INTERPOSE(dwarfmode_hook, feed);
+IMPLEMENT_VMETHOD_INTERPOSE(dwarfmode_hook, render);
+
+
+DFhackCExport command_result plugin_init(color_ostream& out, vector <PluginCommand>& commands)
+{
+    //Reverts back dig designation if damp stone is detected and unpauses the game.
+    //Caution! Use only when digging in aquifer.
+    //Saves all dig designated tiles every frame and rollbacks the designation when it detects new DIG_DAMP_STONE announcement.
+    //This, however, wont restore dwarf dig job, so a dwarf can be confused sometimes before he picks up the restored dig designation again.
+    //(its basically the same if you just reassign the designation youself as usual)
+    commands.push_back(PluginCommand("nodigcancel", "Disable dig cancellation on damp stone (useful when digging in aquifer)", nodigcancel, false,
+        "Disable dig cancellation on damp stone (useful when digging in aquifer)\n"
+        "Activate with 'nodigcancel 1', deactivate with 'nodigcancel 0'.\n"));
+
+    if (!INTERPOSE_HOOK(dwarfmode_hook, render).apply() ||
+        !INTERPOSE_HOOK(dwarfmode_hook, feed).apply())
+        return CR_FAILURE;
+
+    EventManager::registerListener(EventManager::EventType::JOB_COMPLETED, digHandler, plugin_self);
+
+    is_enabled = true;
+    return CR_OK;
+}
+
+DFhackCExport command_result plugin_onupdate(color_ostream& out)
+{
+    if (nodigcancel_state)
+    {
+        t_gamemodes gm;
+        World::ReadGameMode(gm);
+        if (gm.g_mode == game_mode::DWARF)
+        {
+            nodigcancel_update(out);
+        }
+    }
+    return CR_OK;
+}
+
+DFhackCExport command_result plugin_shutdown(color_ostream& out)
+{
+    INTERPOSE_HOOK(dwarfmode_hook, render).remove();
+    INTERPOSE_HOOK(dwarfmode_hook, feed).remove();
+    EventManager::unregisterAll(plugin_self);
+    return CR_OK;        
+}
 
